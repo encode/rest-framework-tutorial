eaed235f2dcff966905deba83cb5a16158d2aba7
Richard Wackerbarth <richard@NFSNet.org>
2013-01-12 13:40:58 -0600

    Artifacts from startproject

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -115,6 +115,10 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Uncomment the next line to enable the admin:
+    # 'django.contrib.admin',
+    # Uncomment the next line to enable admin documentation:
+    # 'django.contrib.admindocs',
     'rest_framework',
     'snippets',
 )
