87cf4c5ff97edd199c798232d80c8b3699537665
Tom Christie <tom@tomchristie.com>
2013-01-15 09:36:26 +0000

    Fix for pygments 1.6rc.
    See also: https://github.com/tomchristie/django-rest-framework/issues/581

--- snippets/models.py
+++ snippets/models.py
@@ -4,8 +4,9 @@ from pygments.styles import get_all_styles
 from pygments.formatters import HtmlFormatter
 from pygments import highlight
 
-LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in get_all_lexers()])
-STYLE_CHOICES = sorted((item, item) for item in list(get_all_styles()))
+LEXERS = [item for item in get_all_lexers() if item[1]]
+LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
+STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
 
 
 class Snippet(models.Model):
