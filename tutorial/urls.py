a85b0479e71d83fa550d3cc373a9fb2197891cbe
Richard Wackerbarth <richard@NFSNet.org>
2013-01-19 12:36:02 -0600

    From start project

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,6 +1,10 @@
 from snippets import views
-from django.conf.urls import patterns, url, include
+from django.conf.urls import patterns, include, url
 from rest_framework.urlpatterns import format_suffix_patterns
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
 
 urlpatterns = format_suffix_patterns(patterns('snippets.views',
     url(r'^$', 'api_root'),
@@ -12,5 +16,15 @@ urlpatterns = format_suffix_patterns(patterns('snippets.views',
 ))
 
 urlpatterns += patterns('',
+    # Examples:
+    # url(r'^$', 'tutorial.views.home', name='home'),
+    # url(r'^tutorial/', include('tutorial.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 )
