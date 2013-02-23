b6fb1b3ad9280211d0644cab854edba0d9bf68a0
Tom Christie <tom@tomchristie.com>
2013-02-23 21:40:23 +0000

    Add missing `blank=True`.

--- snippets/models.py
+++ snippets/models.py
@@ -11,7 +11,7 @@ STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
 
 class Snippet(models.Model):
     created = models.DateTimeField(auto_now_add=True)
-    title = models.CharField(max_length=100, default='')
+    title = models.CharField(max_length=100, blank=True, default='')
     code = models.TextField()
     linenos = models.BooleanField(default=False)
     language = models.CharField(choices=LANGUAGE_CHOICES,
