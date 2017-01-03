f699fb13b1835ae5355403a2352f2575fbb2738a
Chad Smith <chad.smith@zee.aero>
2017-01-03 11:06:28 -0800

    update to Django 1.10 and DRF 3.5.3

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,12 +1,21 @@
-from snippets import views
-from django.conf.urls import patterns, url, include
-from rest_framework.routers import DefaultRouter
+"""tutorial URL Configuration
 
-router = DefaultRouter()
-router.register(r'snippets', views.SnippetViewSet)
-router.register(r'users', views.UserViewSet)
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
 
-urlpatterns = patterns('',
-    url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-)
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+]
