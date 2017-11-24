7bb7fa60244138fdea6173bf5a414bbdb8add880
José Padilla <jpadilla@webapplicate.com>
2017-11-24 13:56:32 -0500

    Add interactive API docs

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import include, url
 from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
 
-schema_view = get_schema_view(title='Pastebin API')
+API_TITLE = 'Pastebin API'
+API_DESCRIPTION = 'A Web API for creating and viewing highlighted code snippets.'
+schema_view = get_schema_view(title=API_TITLE)
 
 urlpatterns = [
     url(r'^', include('snippets.urls')),
-    url(r'^api-auth/',
-        include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^schema/$', schema_view),
+    url(r'^docs/', include_docs_urls(title=API_TITLE, description=API_DESCRIPTION))
 ]
