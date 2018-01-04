2a2503d6d99cdf3290fbaaf30523e7793cb99a79
José Padilla <jpadilla@webapplicate.com>
2018-01-04 13:12:23 -0500

    Add snippets fixtures

--- restore.sh
+++ restore.sh
@@ -6,4 +6,7 @@ python manage.py flush --noinput
 echo "==> Loading user fixtures..."
 python manage.py loaddata snippets/fixtures/users.json
 
+echo "==> Loading snippets fixtures..."
+python manage.py loaddata snippets/fixtures/snippets.json
+
 echo "==> Done!"
