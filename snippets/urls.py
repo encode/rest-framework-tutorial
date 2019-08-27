cb84550a9b02a9f2b31c6a77b91f2c0c27e3518d
Pramochete <orientedresearch@gmail.com>
2019-08-27 22:50:49 +0545

    use path instead of url and is based on latest drf-tutorial

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import include, url
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from snippets import views
@@ -11,5 +11,5 @@ router.register(r'users', views.UserViewSet)
 # The API URLs are now determined automatically by the router.
 # Additionally, we include the login URLs for the browsable API.
 urlpatterns = [
-    url(r'^', include(router.urls))
+    path('', include(router.urls))
 ]
