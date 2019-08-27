f4e58e5de8c0929619c16920c15ec1a7901befa1
Pramochete <orientedresearch@gmail.com>
2019-08-27 22:52:15 +0545

    use action decorator as referred in tutorial

--- snippets/views.py
+++ snippets/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import permissions, renderers, viewsets
-from rest_framework.decorators import detail_route
+from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from snippets.models import Snippet
@@ -10,18 +10,18 @@ from snippets.serializers import SnippetSerializer, UserSerializer
 
 class SnippetViewSet(viewsets.ModelViewSet):
     """
-    This viewset automatically provides `list`, `create`, `retrieve`,
+    This viewset automatically provides `list`, `create', `retrieve`,
     `update` and `destroy` actions.
 
     Additionally we also provide an extra `highlight` action.
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = (
+    permission_classes = [
         permissions.IsAuthenticatedOrReadOnly,
-        IsOwnerOrReadOnly, )
-
-    @detail_route(renderer_classes=[renderers.StaticHTMLRenderer])
+        IsOwnerOrReadOnly]
+    
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
     def highlight(self, request, *args, **kwargs):
         snippet = self.get_object()
         return Response(snippet.highlighted)
