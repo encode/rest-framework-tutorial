8104454135bdef5d730f497bf0cfd9bc28502172
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-01-29 17:21:05 +0100

    Change list comprehensions to generator expressions

--- snippets/models.py
+++ snippets/models.py
@@ -4,8 +4,8 @@ from pygments.formatters.html import HtmlFormatter
 from pygments.lexers import get_all_lexers, get_lexer_by_name
 from pygments.styles import get_all_styles
 
-LEXERS = [item for item in get_all_lexers() if item[1]]
-LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
+LEXERS = (item for item in get_all_lexers() if item[1])
+LANGUAGE_CHOICES = sorted((item[1][0], item[0]) for item in LEXERS)
 STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
 
 
