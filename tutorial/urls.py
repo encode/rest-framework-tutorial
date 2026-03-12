7e9a792829c3525354165df9febb602025690996
Bruno Alla <alla.brunoo@gmail.com>
2026-03-12 19:16:07 +0100

    Replace coreapi by drf-spectacular for API docs

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,14 +1,9 @@
 from django.urls import include, path
-from rest_framework.schemas import get_schema_view
-from rest_framework.documentation import include_docs_urls
-
-API_TITLE = 'Pastebin API'
-API_DESCRIPTION = 'A Web API for creating and viewing highlighted code snippets.'
-schema_view = get_schema_view(title=API_TITLE)
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('', include('snippets.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('schema/', schema_view),
-    path('docs/', include_docs_urls(title=API_TITLE, description=API_DESCRIPTION))
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('docs/', SpectacularSwaggerView.as_view(url_name='schema'))
 ]
